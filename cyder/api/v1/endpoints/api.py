38984f950686928ec4666910fd54162abf703061
Zane Salvatore <zeeman13@gmail.com>
2013-09-19 23:31:11 +0000

    Make Domain read-write.

--- cyder/api/v1/endpoints/api.py
+++ cyder/api/v1/endpoints/api.py
@@ -4,6 +4,61 @@ from rest_framework import serializers, viewsets
 NestedKeyValueFields = ['id', 'key', 'value', 'is_quoted']
 
 
+class NestedFieldSerializer(serializers.ModelSerializer):
+    """Custom API serializer to handle nested many-to-many fields because DRF
+    is kind of dumb and tries to do Domain(..., ctnr_set=things). To use this
+    serializer, you should add an attribute to your serializer's Meta class
+    called `nested_fields` containing a list of string names of many-to-many
+    fields in the serializer's model.
+    """
+    def restore_object(self, attrs, instance=None, *args, **kwargs):
+        """This method does all the work of creating an object representation
+        of the serialized data and saving it/modifying the existing object.
+
+        Params:
+            attrs - A dict of the data sent to the API. Objects passed as links
+                    are automatically turned into object representations which
+                    can be passed to Django directly.
+            instance - If the request is trying to update an object, this
+                       contains the object to be modified.
+        """
+        if not self.is_valid():
+            return self.errors
+
+        if instance is not None:
+            # add nested values from many-to-fields as appropriate
+            if getattr(self.Meta(), 'nested_fields', None):
+                for nested_field in self.Meta().nested_fields:
+                    if nested_field in attrs:
+                        for item in attrs.pop(nested_field):
+                            getattr(self.instance, nested_field).add(item)
+            for field in self.fields:
+                setattr(instance, field, attrs.get(
+                    field, getattr(instance, field)))
+            instance.save()
+            return instance
+
+        if getattr(self.Meta(), 'nested_fields', None):
+            nested_fields = {}
+            for nested_field in self.Meta().nested_fields:
+                if nested_field in attrs:
+                    nested_fields[nested_field] = attrs.pop(nested_field)
+        instance = self.Meta().model(**attrs)
+        instance.save()
+
+        for nested_field in nested_fields:
+            for item in nested_fields[nested_field]:
+                getattr(instance, nested_field).add(item)
+        return instance
+
+    def save_object(self, *args, **kwargs):
+        """Normally this is used to save the object, but obviously that
+        doesn't matter when we save in restore_object(). We're overriding
+        this method to keep DRF from trying to save the already saved object.
+        """
+        pass
+
+
 class CommonAPISerializer(serializers.ModelSerializer):
     pass
 
--- cyder/api/v1/endpoints/dns/domain/api.py
+++ cyder/api/v1/endpoints/dns/domain/api.py
@@ -1,42 +1,20 @@
 from rest_framework import serializers
 
+from cyder.api.v1.endpoints.api import NestedFieldSerializer
 from cyder.api.v1.endpoints.dns import api
 from cyder.cydns.domain.models import Domain
 
 
-class DomainSerializer(serializers.ModelSerializer):
+class DomainSerializer(NestedFieldSerializer):
     id = serializers.Field(source='id')
     master_domain = serializers.HyperlinkedRelatedField(
         many=False, read_only=False, view_name='api-dns-domain-detail')
     ctnr_set = serializers.HyperlinkedRelatedField(
         many=True, read_only=False, view_name='api-core-ctnr-detail')
 
-    def restore_object(self, attrs, instance=None):
-        if not self.is_valid():
-            return self.errors
-
-        if instance is not None:
-            if 'ctnr_set' in attrs:
-                ctnr_set = attrs.pop('ctnr_set')
-                for c in ctnr_set:
-                    self.instance.ctnr_set.add(c)
-            for field in self.fields:
-                setattr(instance, field, attrs.get(
-                    field, getattr(instance, field)))
-            instance.save()
-
-        ctnr_set = attrs.pop('ctnr_set', None)
-        d = Domain(**attrs)
-        d.save()
-        if ctnr_set:
-            for c in ctnr_set:
-                d.ctnr_set.add(c)
-
-    # Looks like we need to override save_object() and full_clean() to make
-    # this work. Otherwise, when DRF tries to save() it screws up the process.
-
     class Meta(api.CommonDNSMeta):
         model = Domain
+        nested_fields = ['ctnr_set']
 
 
 class DomainViewSet(api.CommonDNSViewSet):
