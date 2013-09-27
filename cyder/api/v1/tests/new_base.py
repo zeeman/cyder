0f3709c24ef9e480e2a2ecb5ab1d4cf5d49fc753
Zane Salvatore <zeeman13@gmail.com>
2013-09-27 14:50:59 -0700

    Small change of no consequence.

--- cyder/api/v1/tests/new_base.py
+++ cyder/api/v1/tests/new_base.py
@@ -3,6 +3,10 @@ from django.contrib.auth.models import User
 from rest_framework.test import APIClient
 
 from cyder.api.authtoken.models import Token
+from cyder.base.constants import (ACTION_CREATE, ACTION_DELETE, ACTION_UPDATE,
+                                  ACTION_VIEW)
+from cyder.core.ctnr.models import Ctnr
+from cyder.core.cyuser.backends import _has_perm
 from cyder.cydns.domain.models import Domain
 from cyder.cydns.nameserver.models import Nameserver
 from cyder.cydns.soa.models import SOA
@@ -72,30 +76,85 @@ class APITests(object):
         self.object_url = lambda slug: self.f_object_url.format(
             API_VERSION, root, urlname, slug)
 
-    def assertEqualKeys(self, a, b):
+    # ABSTRACT FUNCTIONS
+    def generate_related_data(self):
+        """This method should return a dict of tuples of form
+            (Related Object Model, Related Object Data)
+        """
+        pass
+
+    def generate_data(self):
+        """This method should return a dict representing core object data."""
+        pass
+
+    def create_related_objects(self):
+        """Creates related objects from generate_related_data."""
+        pass
+
+    def create_object(self):
+        """Creates core object from generate_data and create_related_objects."""
+        pass
+    # END ABSTRACT FUNCTIONS
+
+    @staticmethod
+    def assertEqualKeys(a, b):
         for key in a:
             assert a[key] == b[key]
 
-    def assertHttpOK(self, resp):
+    @staticmethod
+    def assertHttpOK(resp):
         assert resp.status_code == 200
 
-    def assertHttpUnauthorized(self, resp):
+    @staticmethod
+    def assertHttpUnauthorized(resp):
         assert resp.status_code == 401
 
-    def assertHttpNotFound(self, resp):
+    @staticmethod
+    def assertHttpNotFound(resp):
+        assert resp.status_code == 404
+
+    @staticmethod
+    def assertHttpMethodNotAllowed(resp):
         assert resp.status_code == 405
+
+    def has_perm(self, user, action):
+        if not isinstance(user, User):
+            user_obj = User.objects.get(username=user)
+        return _has_perm(
+            user_obj,
+            Ctnr.objects.get(pk__in=user_obj.ctnruser_set.
+                             values_list('ctnr', flat=True)),
+            action,
+            obj_class=self.model)
 
     def load_credentials(self, username):
         self.user = User.objects.get(username=username)
         self.token = Token.objects.get_or_create(user=self.user).key
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token)
 
     def test_permissions(self):
         """This test iterates through each test user and test method to verify
         that permissions are functioning correctly.
         """
-        methods = ['api_get', 'api_post', 'api_put', 'api_patch', 'api_delete']
+        methods = ['api_get_nonexistent', 'api_get_existing', 'api_get_root',
+                   'api_get_list', 'api_get_detail', 'api_post_valid',
+                   'api_post_invalid', 'api_put_valid', 'api_put_invalid',
+                   'api_patch_valid', 'api_patch_invalid', 'api_delete']
         users = ['test_superuser', 'test_admin', 'test_user', 'test_guest']
         for u in users:
+            self.load_credentials(u)
             for m in methods:
-                pass
-        pass
+                getattr(self, m).description = "{0} as {1}".format(m, u)
+                yield getattr(self, m)()
+
+    def api_get_nonexistent(self):
+        obj = self.create_object()
+        bad_id = obj.id
+        obj.delete()
+        resp = self.client.get(self.object_url(bad_id))
+        (self.has_perm(self.user, ACTION_VIEW)
+            and self.assertHttpNotFound(resp)
+            or self.assertHttpUnauthorized(resp))
+
+    def api_get_existing(self):
+        obj = self.create_object()
