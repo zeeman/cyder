2021b548bb3b8c553522500b86c6163cf226c56b
Zane Salvatore <zeeman13@gmail.com>
2014-09-24 18:15:25 -0700

    Add (mysteriously non-functional) CNAME detail view

--- cyder/cydns/cname/urls.py
+++ cyder/cydns/cname/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls.defaults import *
 
 from cyder.cydns.urls import cydns_urls
+from cyder.cydns.cname.views import cname_detail
 
 
-urlpatterns = cydns_urls('cname')
+urlpatterns = cydns_urls('cname') + patterns(
+    '',
+    url(r'(?P<pk>[\w-]+)/$', cname_detail, name='cname-detail'),
+)
